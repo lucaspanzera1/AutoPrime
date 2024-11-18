7df3404bca89d27c97c00b8701fc8cc60135d1d2
LucasPanzera <lucassouzapanzera@gmail.com>
2024-11-18 18:02:32 -0300

    Página de comentários

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,3 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
